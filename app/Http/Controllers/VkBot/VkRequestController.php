3502858f43aa7620c05e6a70be724e109885a760
Vasliliy <vasyokbelyiv@gmail.com>
2019-04-29 18:39:27 +0500

    Implement confirmation

--- app/Http/Controllers/VkBot/VkRequestController.php
+++ app/Http/Controllers/VkBot/VkRequestController.php
@@ -5,8 +5,39 @@ namespace App\Http\Controllers\VkBot;
 use App\Http\Controllers\Controller;
 
 class VkRequestController extends Controller {
+    private $groups = [
+        '174017128' => [
+            'confirm' => '0ce90217',
+            'secret_key'  => 'tZQLjd4ay94P9BWphedT',
+        ],
+    ];
+    
     public function handle() {
-        \Log::info($_POST);
+        $request = json_decode(file_get_contents('php://input'));
+
+        if (!(array_key_exists('type', $request) && array_key_exists('group_id', $request))) {
+            return 'Malformed JSON request';
+        }
+
+        $group_id = $request['group_id'];
+        
+        switch (strtolower($request['type'])) {
+            case 'confirmation':
+                return handleConfirmation($group_id);
+
+            default:
+                \Log::info('Unknown vk request type: '. $request['type']);
+                return 'Unknown type';
+        }
+    }
+
+    private function handleConfirmation($group_id) {
+        if (!array_key_exists($group_id, $groups)) {
+            \Log::info('Unknown vk group: '. $group_id);
+            return 'Unknown group';
+        }
+
+        return $groups[$group_id]['confirm'];
     }
 }
 
