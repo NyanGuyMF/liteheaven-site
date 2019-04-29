b3e64532e3d0bbf198220410567954c09da08431
Vasliliy <vasyokbelyiv@gmail.com>
2019-04-29 18:02:39 +0500

    Fix controller namespace error

--- app/Http/Controllers/VkBot/VkRequestController.php
+++ app/Http/Controllers/VkBot/VkRequestController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\VkBot;
 
+use App\Http\Controllers\Controller;
+
 class VkRequestConsroller extends Controller {
     public function handle() {
         \Log::info($_POST);
