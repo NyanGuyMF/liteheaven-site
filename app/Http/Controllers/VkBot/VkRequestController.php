7b0fac5474f8d3fadca26532894c865308c8d088
Vasliliy <vasyokbelyiv@gmail.com>
2019-05-01 14:31:31 +0500

    Refactoring

--- app/Http/Controllers/VkBot/VkRequestController.php
+++ app/Http/Controllers/VkBot/VkRequestController.php
@@ -5,23 +5,12 @@ namespace App\Http\Controllers\VkBot;
 use App\Http\Controllers\Controller;
 
 class VkRequestController extends Controller {
-    private $groups = [
-        '174017128' => [
-            'confirm' => '0ce90217',
-            'secret_key'  => 'tZQLjd4ay94P9BWphedT',
-        ],
-    ];
-
     public function handle() {
         $request = json_decode(file_get_contents('php://input'), true);
 
-        if (!(array_key_exists('type', $request) && array_key_exists('group_id', $request))) {
-            return 'Malformed JSON request';
-        }
-
         $group_id = $request['group_id'];
 
-        switch (strtolower($request['type'])) {
+        switch ( strtolower($request['type']) ) {
             case 'confirmation':
                 return self::handleConfirmation($group_id);
             case 'message_new':
@@ -34,12 +23,7 @@ class VkRequestController extends Controller {
     }
 
     private function handleConfirmation($group_id) {
-        if (!array_key_exists($group_id, $this->groups)) {
-            \Log::info('Unknown vk group: '. $group_id);
-            return 'Unknown group';
-        }
-
-        return $this->groups[$group_id]['confirm'];
+        return config('app.vkbot.groups')[$group_id]['confirm'];
     }
 }
 
